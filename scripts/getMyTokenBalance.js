a802bbe22af4ca78146c0fe3f199ba81dc7a861c
Kai Roehrig <kairoehrig@gmail.com>
2021-06-20 00:49:08 +0100

    display mytoken balances for all accounts

--- scripts/getMyTokenBalance.js
+++ scripts/getMyTokenBalance.js
@@ -2,9 +2,17 @@ const MyToken = artifacts.require("MyToken")
 const TipRouter = artifacts.require("TipRouter")
 
 module.exports = async function (callback) {
+    const accounts = await new web3.eth.getAccounts()
     myToken = await MyToken.deployed()
     tipRouter = await TipRouter.deployed()
+
     balance = await myToken.balanceOf(tipRouter.address)
-    console.log(web3.utils.fromWei(balance.toString()))
+    console.log("Balance of tipRouter: " + web3.utils.fromWei(balance.toString()))
+
+    for (let i = 0; i < accounts.length; i++){
+        balance = await myToken.balanceOf(accounts[i])
+        console.log("Balance of account " + i.toString() + ": " + web3.utils.fromWei(balance.toString()))
+    }
+
     callback()
 }
